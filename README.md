4d748e0dd58d96bbe24a23fc850a3f8d779eea21
Younes Lahouiti <loller763@gmail.com>
2023-05-06 11:18:38 +0200

    Addition of the TS utility functions

--- README.md
+++ README.md
@@ -24,9 +24,7 @@ Background Effects is a mini-personal project that aims to provide users with a 
 <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer" title="HTML5"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg" width="36" height="36" alt="HTML5" /></a>
 <a href="https://sass-lang.com/" target="_blank" rel="noreferrer" title="SASS"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/sass-colored.svg" width="36" height="36" alt="Sass" /></a>
 <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer" title="TypeScript"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg" width="36" height="36" alt="TypeScript" /></a>
-
-
-This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+s
 
 ## 3. Features
 
---
+++ src/utils/functions/number.functions.ts
@@ -0,0 +1,39 @@
+/**
+ * Generates a random number within a specified range.
+ * @param {number} min - The minimum value of the range.
+ * @param {number} max - The maximum value of the range.
+ * @param {boolean} includeMin - Whether to include the minimum value in the range.
+ * @param {boolean} includeMax - Whether to include the maximum value in the range.
+ *
+ * @returns {number} A random number within the specified range.
+ */
+function getRandomNumber(
+  min: number,
+  max: number,
+  includeMin: boolean = true,
+  includeMax: boolean = true
+): number {
+  const hasInvalidArgument: boolean = min > max || max < min;
+  if (hasInvalidArgument) {
+    throw new Error(
+      `Unexpected error occured in the passed argument values: min > max or max < min`
+    );
+  }
+
+  const mustIncludeBoth: boolean = includeMin && includeMax;
+
+  const mustIncludeOnlyMin: boolean = includeMin && !includeMax;
+
+  const mustIncludeOnlyMax: boolean = !includeMin && includeMax;
+
+  if (mustIncludeBoth) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else if (mustIncludeOnlyMin) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  } else if (mustIncludeOnlyMax) {
+    return Math.floor(Math.random() * (max - min)) + min + 1;
+  } else {
+    //We don't include either
+    return Math.floor(Math.random() * (max - min - 1)) + min + 1;
+  }
+}
