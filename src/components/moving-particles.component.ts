62e958971de5c42bde5188dfdcbabd7db17635ac
Younes Lahouiti <loller763@gmail.com>
2023-05-07 10:34:55 +0200

    Addition of the JSDoc for our moving particles web component, finalizing the effect on that WC, now starting to create the Bouncing balls component

--- src/components/moving-particles.component.ts
+++ src/components/moving-particles.component.ts
@@ -48,14 +48,40 @@ movingParticlesTemplateElement.innerHTML = /*html */ `
   ${movingParticlesTemplateHtml}
 `;
 
+/**
+ * A custom element that creates and animates moving particles in a canvas element.
+ * @extends HTMLElement
+ */
 class MovingParticles extends HTMLElement {
+  /**
+   * The canvas element used for rendering the particles.
+   * @type {HTMLCanvasElement}
+   */
   canvas: HTMLCanvasElement;
 
+  /**
+   * The 2D context of the canvas element.
+   * @type {CanvasRenderingContext2D}
+   */
   context: CanvasRenderingContext2D;
 
+  /**
+   * The object responsible for creating and animating the particles.
+   * @type {MovingParticlesCreator}
+   */
   effectHandler: MovingParticlesCreator;
 
+  /**
+   * The ID of the current animation frame. Useful to cancel the animation loop when needed
+   * @type {number}
+   */
   animationId: number;
+
+  /**
+   * Creates a new instance of the `MovingParticles` class.
+   *
+   * @constructor
+   */
 
   constructor() {
     super();
@@ -63,9 +89,17 @@ class MovingParticles extends HTMLElement {
     // bind the context of animateCanvas to this
     /**
      * We bind the `this` keyword with the `animateCanvas()` method
-     * to make sure that the method has access to its values
+     * to make sure that the method has access to its values.
+     * @function
      */
     this.animateCanvas = this.animateCanvas.bind(this);
+
+    /**
+     * We bind the `this` keyword with the `resetCanvasToMatchScreen()` method
+     * to make sure that the method has access to its values.
+     * @function
+     */
+    this.resetCanvasToMatchScreen = this.resetCanvasToMatchScreen.bind(this);
 
     const shadowRoot: ShadowRoot = this.attachShadow({ mode: "open" });
 
@@ -75,45 +109,96 @@ class MovingParticles extends HTMLElement {
     //We add it as a child of our web component
     shadowRoot.appendChild(clonedTemplate);
 
+    /**
+     * The canvas element used for rendering the particles.
+     * @type {HTMLCanvasElement}
+     */
     this.canvas = selectQuery("canvas", this.shadowRoot);
+
+    /**
+     * The 2D context of the canvas element.
+     * @type {CanvasRenderingContext2D}
+     */
     this.context = get2DContext(this.canvas);
 
+    /**
+     * The object responsible for creating and animating the particles.
+     * @type {MovingParticlesCreator}
+     */
     this.effectHandler = new MovingParticlesCreator(this.canvas, 100);
     this.resizeCanvas();
   }
 
-  static get observedAttributes() {
+  /**
+   * An array of attribute names to observe for changes.
+   * @returns {string[]}
+   * @static
+   */
+  static get observedAttributes(): string[] {
     //We indicate the list of attributes that the custom element wants to observe for changes.
     return ["is-playing"];
   }
 
-  get isPlaying(): any {
+  /**
+   * Getter returing the current value of the `is-playing` attribute.
+   * @returns {boolean}
+   */
+  get isPlaying(): boolean {
     const attribute = this.getAttribute("is-playing");
     return JSON.parse(attribute);
   }
-  set isPlaying(value: any) {
+
+  /**
+   * Setter that changes the current value of the `is-playing` attribute.
+   * @param {boolean} value - The new value of the `is-playing` attribute.
+   */
+  set isPlaying(value: boolean) {
     const stringifiedValue = value.toString();
     this.setAttribute("is-playing", stringifiedValue);
   }
 
+  /**
+   * Called when the element is inserted into the DOM.
+   */
   connectedCallback() {
     this.effectHandler = new MovingParticlesCreator(this.canvas, 100);
-    log(this.canvas, this.context, this.effectHandler);
 
     setCanvasSize(this.canvas, this.clientWidth, this.clientHeight);
 
-    window.addEventListener("resize", () => {
-      //We're using a callback function to have access to the width and height of our web component
-      setCanvasSize(this.canvas, this.clientWidth, this.clientHeight);
-      this.cancelCanvasAnimation();
-      this.effectHandler = new MovingParticlesCreator(this.canvas, 100);
-      this.animateCanvas();
-    });
+    window.addEventListener("resize", this.resetCanvasToMatchScreen);
   }
 
-  resizeCanvas() {}
+  /**
+   * Resizes the canvas element to match the current screen size.
+   *
+   * @returns {void}
+   */
+  resizeCanvas(): void {
+    setCanvasSize(this.canvas, this.clientWidth, this.clientHeight);
+  }
 
-  animateCanvas() {
+  /**
+   * Resets the canvas element to match the current screen size and restarts the particles animation.
+   *
+   * @returns {void}
+   */
+  resetCanvasToMatchScreen(): void {
+    //We set a new width and height to our canvas
+    this.resizeCanvas();
+    //We cancel the animation loop
+    this.cancelCanvasAnimation();
+    //We create a new effect
+    this.effectHandler = new MovingParticlesCreator(this.canvas, 100);
+    //We restart the animation loop
+    this.animateCanvas();
+  }
+
+  /**
+   * Animates the particles effect on the canvas element.
+   *
+   * @returns {void}
+   */
+  animateCanvas(): void {
     this.context.fillStyle = `rgba(0,0,0,10%)`;
 
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
@@ -123,18 +208,38 @@ class MovingParticles extends HTMLElement {
     this.animationId = requestAnimationFrame(this.animateCanvas);
   }
 
-  cancelCanvasAnimation() {
-    log(this.animationId);
+  /**
+   * Cancels the current animation frame request to stop the animation loop.
+   *
+   * @returns {void}
+   */
+  cancelCanvasAnimation(): void {
     cancelAnimationFrame(this.animationId);
   }
 
-  disconnectedCallback() {
-    window.removeEventListener("resize", () => {
-      this.resizeCanvas();
-    });
+  /**
+   * Called when the element is remove from the DOM.
+   *
+   * @returns {void}
+   */
+  disconnectedCallback(): void {
+    window.removeEventListener("resize", this.resetCanvasToMatchScreen);
   }
 
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+  /**
+   * Method called whenever an attribute is added, changed or deleted from our web component
+   *
+   * @param {string} name - Name of the attribute that changed
+   * @param {string} oldValue - Old value of the attribute before a change
+   * @param {string} newValue - New value of the attribute after a change
+   *
+   * @returns {void}
+   */
+  attributeChangedCallback(
+    name: string,
+    oldValue: string,
+    newValue: string
+  ): void {
     switch (name) {
       case "is-playing": {
         const isPlaying: boolean = newValue === "true";
