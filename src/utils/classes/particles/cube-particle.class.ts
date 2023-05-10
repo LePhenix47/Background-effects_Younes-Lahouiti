d1113cedc64e26ac46fddd5ca67007b07df32017
Younes Lahouiti <loller763@gmail.com>
2023-05-10 10:12:25 +0200

    Fixed the layout UI + init of the cube warp effect

--- src/utils/classes/particles/cube-particle.class.ts
+++ src/utils/classes/particles/cube-particle.class.ts
@@ -13,6 +13,7 @@ export class CubeParticle {
   y: number;
   private length: number;
   private rotation: number;
+  growthExponent: number;
 
   /**
    * Creates a new instance of the `CubeParticle` class.
@@ -32,6 +33,7 @@ export class CubeParticle {
     this.height = height;
 
     this.length = 1;
+    this.growthExponent = 0;
 
     this.x = this.width / 2;
     this.y = this.height / 2;
@@ -43,8 +45,10 @@ export class CubeParticle {
    * Updates the state of the cube particle.
    */
   update() {
-    this.length += 1;
+    this.length += 1 + this.growthExponent;
     this.rotation += 0.1;
+
+    this.growthExponent++;
 
     this.checkGrowthOverflow();
 
@@ -58,6 +62,7 @@ export class CubeParticle {
     const isTooBig = this.length >= this.width && this.height;
     if (isTooBig) {
       this.length = 1;
+      this.growthExponent = 0;
     }
   }
 
@@ -65,12 +70,24 @@ export class CubeParticle {
    * Draws the cube particle on the canvas.
    */
   draw() {
-    this.context.strokeStyle = `white`;
+    this.context.save();
+
+    // Translate to the center of the canvas
+    this.context.translate(this.width / 2, this.height / 2);
+
+    // Rotate the canvas by the rotation angle in radians
+    this.context.rotate((this.rotation * Math.PI) / 180);
+
+    // Set the stroke color and width for the square
+    this.context.strokeStyle = "white";
+    this.context.lineWidth = 5;
 
     //Must use variables (not class properties) to compute the center of the square
-    const middleSquareX: number = this.x - this.length / 2;
-    const middleSquareY: number = this.y - this.length / 2;
+    const middleSquareX: number = (-1 * this.length) / 2;
+    const middleSquareY: number = (-1 * this.length) / 2;
 
+    this.context.strokeStyle = `white`;
+    // Draw the square with a stroke
     this.context.strokeRect(
       middleSquareX,
       middleSquareY,
@@ -78,6 +95,6 @@ export class CubeParticle {
       this.length
     );
 
-    this.context.stroke();
+    this.context.restore();
   }
 }
